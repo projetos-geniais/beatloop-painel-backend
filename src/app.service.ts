6f353b0774dce43c338f4ce10436d2a569c1d3ea
Flávio Peres <flaviopc2@gmail.com>
2025-02-01 12:46:41 -0300

    enviando arquivo .env.examplo

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService implements OnModuleInit {
     setInterval(async () => {
       try {
         // Enviar um "ping" para a rota /health para garantir que a API está ativa
-        await axios.get('http://localhost:3000');
+        await axios.get('https://painelbeatloop-backend.onrender.com');
         console.log('Ping enviado para manter a API ativa');
       } catch (error) {
         console.error('Erro ao enviar ping:', error);
