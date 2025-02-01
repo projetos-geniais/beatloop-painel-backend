e20a293e64e878ce5cbcfffe56746856a8d98f76
Flávio Peres <flaviopc2@gmail.com>
2025-02-01 12:14:48 -0300

    Primeiro commit2

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  async onModuleInit() {
+    setInterval(async () => {
+      try {
+        // Enviar um "ping" para a rota /health para garantir que a API está ativa
+        await axios.get('http://localhost:3000');
+        console.log('Ping enviado para manter a API ativa');
+      } catch (error) {
+        console.error('Erro ao enviar ping:', error);
+      }
+    }, 1000 * 60 * 5); // A cada 5 minutos
   }
 }
